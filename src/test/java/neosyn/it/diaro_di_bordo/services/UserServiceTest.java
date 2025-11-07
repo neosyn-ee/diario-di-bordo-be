48477141f07e398acdc0ba547a12ed23fcb17336
a.mancuso <a.mancuso@nexidigital.eu>
2025-11-07 16:15:16 +0100

    Configure service test like example

--- src/test/java/neosyn/it/diaro_di_bordo/services/UserServiceTest.java
+++ src/test/java/neosyn/it/diaro_di_bordo/services/UserServiceTest.java
@@ -1,15 +1,11 @@
 package neosyn.it.diaro_di_bordo.services;
+import neosyn.it.diaro_di_bordo.model.dto.UserDTO;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.context.SpringBootTest;
-
-import org.springframework.boot.test.mock.mockito.MockBean;
-
-import static org.mockito.Mockito.when;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.Optional;
 
 @SpringBootTest
 public class UserServiceTest {
@@ -17,35 +13,17 @@ public class UserServiceTest {
     @Autowired
     private UserService userService;
 
-    @MockBean
-    private UserRepository userRepository;
-
     @Test
     public void getUserByIDTest() {
 
         // Mocks
         long mockUserId = 123456789L;
 
-        UserEntity mockEntity = new UserEntity();
-        mockEntity.setId(mockUserId);
-        mockEntity.setUsername("John Doe");
-        mockEntity.setPassword("the most secure password in the world");
-        mockEntity.setRole(null);
-
-        // Simula il comportamento del repository
-        when(userRepository.findById(mockUserId)).thenReturn(Optional.of(mockEntity));
-
-        UserDTO mockUser = new UserDTO();
-        mockUser.setId(mockUserId);
-        mockUser.setUsername("John Doe");
-        mockUser.setPassword("the most secure password in the world");
-        mockUser.setRole(null);
-
-        UserDTO expected = mockUser;
+        UserDTO expectedUser = new UserDTO(mockUserId, "John Doe", "the most secure password in the world", null);
 
         UserDTO returned = userService.getUserByID(mockUserId);
 
         // Assert
-        assertEquals(mockUser, returned);
+        assertEquals(expectedUser, returned);
     }
 }
